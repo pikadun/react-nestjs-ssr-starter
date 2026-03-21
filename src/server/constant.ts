ff53b235aa4e57f1f880ac77767a6315a8e6697e
Dunn <wg0121@outlook.com>
2026-03-21 23:00:21 +0800

    refactor: update index.html for dynamic color scheme

--- src/server/constant.ts
+++ src/server/constant.ts
@@ -3,4 +3,3 @@ export const CLIENT_ENTRY_NAME = "index";
 export const STATIC_NAME = "static";
 export const HTML_PLACEHOLDER_BASE = "base";
 export const HTML_PLACEHOLDER_CONTENT = "content";
-export const HTML_PLACEHOLDER_CSS = "css";
